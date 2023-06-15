2ea4293d9d38012401ab1257681b8a03a1520bbe
0ignacio <ignaciogonzaley@gmail.com>
2023-06-15 19:38:39 -0300

    serializacion terminada

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿using clasePersonajes;
+using System.Text.Json;
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
 
 personaje nuevo;
+List<personaje> listaPjs = new List<personaje>();
+List<personaje> listaNueva = new List<personaje>();
 FabricaDePersonajes fp = new FabricaDePersonajes();
-nuevo = fp.crearPersonaje();
-Console.WriteLine(nuevo.Name);
+for (int i = 0; i < 5; i++)
+{
+    nuevo = fp.crearPersonaje();
+    listaPjs.Add(nuevo);
+}
+
+personajesJson pjson = new personajesJson();
+
+if (pjson.existeArchivo("personajes.json"))
+{
+    Console.WriteLine("Existe");
+}else{
+    Console.WriteLine("No Existe");
+}
+
+pjson.guardarPjs(listaPjs);
+
+listaNueva = pjson.leerPersonajes("personajes.json");
