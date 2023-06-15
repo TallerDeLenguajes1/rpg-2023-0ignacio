0a53b4b2f36cfa2b87b99682dc9953767d813fc7
0ignacio <ignaciogonzaley@gmail.com>
2023-06-15 18:30:00 -0300

    segundo commit fabrica personajes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using clasePersonajes;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+personaje nuevo;
+FabricaDePersonajes fp = new FabricaDePersonajes();
+nuevo = fp.crearPersonaje();
+Console.WriteLine(nuevo.Name);
--- personajes.cs
+++ personajes.cs
@@ -1,26 +1,54 @@
-namespace personajes;
+namespace clasePersonajes{
 
-public class personaje{
-    private string? tipo;
-    private string? name;
-    private DateTime fecNac;
-    private int edad;
-    private int vel;
-    private int dest;
-    private int fuerza;
-    private int nivel;
-    private int arm;
-    private int hp;
+    public class personaje{
+        private string? tipo;
+        private string? name;
+        private DateTime fecNac;
+        private int edad;
+        private int vel;
+        private int dest;
+        private int fuerza;
+        private int nivel;
+        private int arm;
+        private int hp;
 
-    public string? Tipo { get => tipo; set => tipo = value; }
-    public string? Name { get => name; set => name = value; }
-    public DateTime FecNac { get => fecNac; set => fecNac = value; }
-    public int Edad { get => edad; set => edad = value; }
-    public int Vel { get => vel; set => vel = value; }
-    public int Dest { get => dest; set => dest = value; }
-    public int Fuerza { get => fuerza; set => fuerza = value; }
-    public int Nivel { get => nivel; set => nivel = value; }
-    public int Arm { get => arm; set => arm = value; }
-    public int Hp { get => hp; set => hp = value; }
-    
+        public string? Tipo { get => tipo; set => tipo = value; }
+        public string? Name { get => name; set => name = value; }
+        public DateTime FecNac { get => fecNac; set => fecNac = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Vel { get => vel; set => vel = value; }
+        public int Dest { get => dest; set => dest = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Arm { get => arm; set => arm = value; }
+        public int Hp { get => hp; set => hp = value; }
+        
+    }
+     
+    public class FabricaDePersonajes{
+        string[] tipos = {"Tanque", "Arquero", "Mago", "Apoyo", "Barbaro"};
+        string[] names = {"Thomas", "Agustin", "Ignacio", "Omar", "Juan"};
+        
+        public personaje crearPersonaje(){
+            personaje nuevo = new personaje();
+            Random rnd = new Random();
+            int auxTipo = rnd.Next(0,5);
+            nuevo.Tipo = tipos[auxTipo];
+            int auxName = rnd.Next(0,5);
+            nuevo.Name = names[auxName];
+            int dia = rnd.Next(1,31);
+            int mes = rnd.Next(1,13);
+            int anio = rnd.Next(1723,2024);
+            //nuevo.FecNac = new DateTime(dia, mes, anio);
+            nuevo.Edad = 2023 - anio;
+            nuevo.Hp = 100;
+            nuevo.Nivel = rnd.Next(1,11);
+            nuevo.Arm = rnd.Next(1,11);
+            nuevo.Dest = rnd.Next(1,6);
+            nuevo.Vel = rnd.Next(1,11);
+            nuevo.Fuerza = rnd.Next(1,11);
+
+            return nuevo;
+        }
+    }
 }
