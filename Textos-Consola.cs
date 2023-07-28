8c6c5adc8462298fc2e264be7f7a96861f77d8d8
0ignacio <ignaciogonzaley@gmail.com>
2023-07-28 05:32:12 -0300

    correcciones generales

--- Textos-Consola.cs
+++ Textos-Consola.cs
@@ -12,7 +12,7 @@ namespace TextosConsola
 
         public static void Menu()
         {
-            Console.WriteLine("RICK & MORTY");
+            Console.WriteLine("\tRICK & MORTY");
             Console.WriteLine("=======================");
             Console.WriteLine("1 - Personajes");
             Console.WriteLine("2 - Iniciar partida");
@@ -20,8 +20,13 @@ namespace TextosConsola
             Console.WriteLine("=======================");
             Console.WriteLine("Ingresar opcion: ");
         }
-        public static void MenuBatalla()
+        public static void MenuBatalla(List<personaje> lista, int Pj, int Nivel)
         {
+            Console.WriteLine($"Batalla Nivel {Nivel}");
+            Console.WriteLine("=======================");
+            Console.WriteLine($"{lista[Pj].Name} --- Lvl {lista[Pj].Nivel} --- {lista[Pj].Hp} Hp");
+            Console.WriteLine("\tVs");
+            Console.WriteLine($"{lista[Nivel+1].Name} --- Lvl {lista[Nivel+1].Nivel} --- {lista[Nivel+1].Hp} Hp");
             Console.WriteLine("=======================");
             Console.WriteLine("1 - Atacar");
             Console.WriteLine("2 - Defender (25%)");
@@ -30,13 +35,13 @@ namespace TextosConsola
         }
         public static void MenuFinal(int resultado)
         {
-            Console.WriteLine("RICK & MORTY");
+            Console.WriteLine("\tRICK & MORTY");
             Console.WriteLine("=======================");
             if (resultado == 1)
             {
-                Console.WriteLine("WINNER");
+                Console.WriteLine("\tWINNER");
             }else{
-                Console.WriteLine("GAME OVER");
+                Console.WriteLine("\tGAME OVER");
             }
             Console.WriteLine("=======================");
             Console.WriteLine("1 - Personajes");
@@ -57,11 +62,11 @@ namespace TextosConsola
         }
         public static void MostrarPersonajes(List<personaje> lista)
         {
-            Console.WriteLine("Personajes");
+            Console.WriteLine("\tPersonajes");
             Console.WriteLine("=======================");
             for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine($"{lista[i].Name}:");
+                Console.WriteLine($"Nombre: {lista[i].Name}:");
                 Console.WriteLine($"Especie: {lista[i].Especie}");
                 Console.WriteLine($"Origen: {lista[i].Origen}");
                 Console.WriteLine("Caracteristicas:");
@@ -77,12 +82,12 @@ namespace TextosConsola
         public static void MostrarEnemigos(List<personaje> lista)
         {
             int aux = 1;
-            Console.WriteLine("Enemigos");
+            Console.WriteLine("\tEnemigos");
             Console.WriteLine("=======================");
             for (int i = 2; i < 7; i++)
             {
                 Console.WriteLine($"Nivel {aux}:");
-                Console.WriteLine($"{lista[i].Name}:");
+                Console.WriteLine($"Nombre: {lista[i].Name}");
                 Console.WriteLine($"Especie: {lista[i].Especie}");
                 Console.WriteLine($"Origen: {lista[i].Origen}");
                 Console.WriteLine("Caracteristicas:");
@@ -113,7 +118,7 @@ namespace TextosConsola
         }
         public static void MostrarPj(List<personaje> lista, int Pj)
         {
-            Console.WriteLine($"Tu personaje: {lista[Pj].Name}:");
+            Console.WriteLine($"Tu personaje: {lista[Pj].Name}");
             Console.WriteLine("=======================");
             Console.WriteLine($"Especie: {lista[Pj].Especie}");
             Console.WriteLine($"Origen: {lista[Pj].Origen}");
@@ -127,17 +132,18 @@ namespace TextosConsola
             Console.WriteLine("=======================");
         }
         public static void PasoNivel(List<personaje> lista, int Pj, int Nivel){
-            if (Nivel !=5)
+            if (Nivel != 6)
             {
                 Console.WriteLine($"Superaste el nivel {Nivel}");
                 Console.WriteLine("=======================");
                 Console.WriteLine("Regenarion de 50 de vida");
                 Console.WriteLine($"Vida: {lista[Pj].Hp}");
+                Console.WriteLine("=======================");
                 Console.WriteLine("Sube 1 nivel");
                 Console.WriteLine($"Nivel: {lista[Pj].Nivel}");
+                Console.WriteLine("=======================");
             }else{
-
-                Console.WriteLine($"FELICIDADES");
+                Console.WriteLine($"\tFELICIDADES");
                 Console.WriteLine($"Superaste todos los niveles");
                 Console.WriteLine("=======================");
                 MostrarPj(lista, Pj);
@@ -146,7 +152,7 @@ namespace TextosConsola
         }
         public static void GameOver(List<personaje> lista, int Pj, int Nivel)
         {
-            Console.WriteLine("GAME OVER");
+            Console.WriteLine("\tGAME OVER");
             Console.WriteLine($"Nivel alcanzado: {Nivel}");
             Console.WriteLine("=======================");
             MostrarPj(lista, Pj);
